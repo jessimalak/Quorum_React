025f13e00434497afda464a041c90520cf8e0d3d
jessimalak <jessimalak21@gmail.com>
2021-01-11 19:08:31 -0500

    light and dark theme

--- classes/Themes.ts
+++ classes/Themes.ts
@@ -30,8 +30,7 @@ export const violeta_light = {
         card: "#7159f7",
         text: "#ffffff",
         border: "#000000",
-        notification: "#b859f7",
-        cardText: "#000000"
+        notification: "#b859f7"
     }
 }
 
@@ -44,6 +43,54 @@ export const violeta_dark = {
         text: "#ffffff",
         border: "#ffffff",
         notification: "#75204f"
+    }
+}
+
+export const rojo_light = {
+    dark: false,
+    colors:{
+        primary: "#a8071a",
+        background: "#ffffff",
+        card: "#ff7875",
+        text: "#ffffff",
+        border: "#000000",
+        notification: "#ff4d4f",
+    }
+}
+
+export const rojo_dark = {
+    dark: true,
+    colors:{
+        primary: "#ff7875",
+        background: "#333333",
+        card: "#a8071a",
+        text: "#ffffff",
+        border: "#ffffff",
+        notification: "#791a1f"
+    }
+}
+
+export const dark = {
+    dark: true,
+    colors:{
+        primary: "#7d7d7d",
+        background: "#000000",
+        card: "#000000",
+        text: "#ffffff",
+        border: "#ffffff",
+        notification: "#333333"
+    }
+}
+
+export const light = {
+    dark: false,
+    colors:{
+        primary: "#c7c7c7",
+        background: "#ffffff",
+        card: "#ffffff",
+        text: "#000000",
+        border: "#000000",
+        notification: "#dddddd"
     }
 }
 
